429a9431455ee38bc96bc1361124a9cb7d1ce91f
tianyang926 <tianyang926@gmail.com>
2018-02-21 02:23:39 +0000

    hdf5 is slow...

--- train-vgg.py
+++ train-vgg.py
@@ -39,18 +39,18 @@ def get_data(data_dir, hdf5):
         if not os.path.exists('hdf5'):
             os.makedirs('hdf5')
         # Check if hdf5 databases already exist and create them if not.
-        # if not os.path.exists('hdf5/tiny-imagenet_train.h5'):
-        from tflearn.data_utils import build_hdf5_image_dataset
+        if not os.path.exists('hdf5/tiny-imagenet_train.h5'):
+            from tflearn.data_utils import build_hdf5_image_dataset
 
-        print ' Creating hdf5 train dataset.'
-        build_hdf5_image_dataset(train_file, image_shape=(224, 224), mode='file',
-                                 output_path='hdf5/tiny-imagenet_train.h5', categorical_labels=True, normalize=True)
+            print ' Creating hdf5 train dataset.'
+            build_hdf5_image_dataset(train_file, image_shape=(224, 224), mode='file',
+                                     output_path='hdf5/tiny-imagenet_train.h5', categorical_labels=True, normalize=True)
 
-        # if not os.path.exists('hdf5/tiny-imagenet_val.h5'):
-        from tflearn.data_utils import build_hdf5_image_dataset
-        print ' Creating hdf5 val dataset.'
-        build_hdf5_image_dataset(val_file, image_shape=(224, 224), mode='file', output_path='hdf5/tiny-imagenet_val.h5',
-                                 categorical_labels=True, normalize=True)
+        if not os.path.exists('hdf5/tiny-imagenet_val.h5'):
+            from tflearn.data_utils import build_hdf5_image_dataset
+            print ' Creating hdf5 val dataset.'
+            build_hdf5_image_dataset(val_file, image_shape=(224, 224), mode='file', output_path='hdf5/tiny-imagenet_val.h5',
+                                     categorical_labels=True, normalize=True)
 
         # Load training data from hdf5 dataset.
         h5f = h5py.File('hdf5/tiny-imagenet_train.h5', 'r')
